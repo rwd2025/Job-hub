875aa1a28eec059c45d3ff582e20d712f7a4c086
rwd2025 <jameswsjacobs87@gmail.com>
2026-04-29 13:21:33 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+Mobile Mechanic Pro V35
+Upload index.html, manifest.json, and service-worker.js to your GitHub Pages root.
+Fixes: filter wiring restored, OCR part-first, AI cache/reset tools added, basic PWA package.
---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="mmp-v35";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
